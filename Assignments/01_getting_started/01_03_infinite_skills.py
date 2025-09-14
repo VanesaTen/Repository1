334d480db8d0acfbf939f4aa6d41fcdaf854aba3
VanesaTen <vanesa.tenorio001@albright.edu>
2025-09-14 19:21:41 -0400

    Add files via upload

---
+++ Assignments/01_getting_started/01_03_infinite_skills.py
@@ -0,0 +1,14 @@
+"""
+
+If I were to gain infinite skills, I would join the world of AI. 
+I believe python has a great connection to AI because it's more simpliar then C++.
+Given that I have some experiance in C++, it makes it easier to understand some code. 
+For example, today in class the professor asked "what does a # in python mean?"
+Without knowing, I kind of assumed it was for comments. C++ has similiar methods for commenting. 
+To use more than one line for a comment in C++ it's /*  */ but one comment line is //. 
+In python, one comment line is # and more than one line.
+
+"""
+
+
+
