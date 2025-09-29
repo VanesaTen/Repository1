a5b04746eb9987143421ae91d854efdc6fa267b7
VanesaTen <vanesa.tenorio001@albright.edu>
2025-09-28 20:23:27 -0400

    Update 05_04_alien_colors_2.py

--- Assignments/05_if_statements/05_04_alien_colors_2.py
+++ Assignments/05_if_statements/05_04_alien_colors_2.py
@@ -1,4 +1,4 @@
-
+# applying true and false statements 
 
 
 alien_colors =  'yellow'
@@ -6,3 +6,4 @@ if 'yellow' in alien_colors:
     print ("You earned 5 points for shooting the aliens!")
 else:
     print("You won 10 points")
+
