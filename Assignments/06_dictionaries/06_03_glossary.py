0d5414ec98818679822552e1f7251cd435cc9e87
VanesaTen <vanesa.tenorio001@albright.edu>
2025-10-06 22:35:05 -0400

    Update 06_03_glossary.py

--- Assignments/06_dictionaries/06_03_glossary.py
+++ Assignments/06_dictionaries/06_03_glossary.py
@@ -1,4 +1,8 @@
+# printing key value pairs
+
 glossary = {'Dictionary':'Dictionary stores lists of variables.','Python': 'your coding in a language.','keyError': 'there is an error in the program.', 'lists': 'there is a list of elements in a dictionary','for_in': 'you can loop through items.'}
 
+# this loops through it
 for key, value in glossary.items():
     print(f"{key} means that {value}\n")
+
