af4d9afdd08cdc16cf21f1cd34a4c5a0a0dca721
VanesaTen <vanesa.tenorio001@albright.edu>
2025-10-06 22:37:13 -0400

    Update 06_09_favorite_places.py

--- Assignments/06_dictionaries/06_09_favorite_places.py
+++ Assignments/06_dictionaries/06_09_favorite_places.py
@@ -1,11 +1,15 @@
+# PEOPLE AND PLACES
 fav_places = {'Josh': ['Paris, Italy, France'], 'Sonia': ['Miami Cancun China'], 'Rudy': ['DisneyLand Area 51 Washington ']}
 
 
+# QUESTION
 people = ['Josh', 'Sonia']  
 for person in people:
     print(f"Hi, {person}, name your favorite places.")
 print('....................')
 
+# LOOPING
 
 for person in fav_places:
     print(f"{person}:{fav_places[person]}")
+
